cd3ecf9eb9f36493b34601e96e8d0bc389970ff0
shasherazi <hassanrandomz@gmail.com>
2023-04-20 14:53:39 +0500

    Add more failing tests for fizzbuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -7,5 +7,17 @@ describe Solver do
     it 'returns fizz on 3' do
       expect(solver.fizzbuzz(3)).to eq('fizz')
     end
+
+    it 'returns buzz on 5' do
+      expect(solver.fizzbuzz(5)).to eq('buzz')
+    end
+
+    it 'returns fizzbuzz on 15' do
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'returns 1 on 1' do
+      expect(solver.fizzbuzz(1)).to eq(1)
+    end
   end
 end
