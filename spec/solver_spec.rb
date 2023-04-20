35e5bf5a8aafc91fe732e7af0e0044c18b4b1619
shasherazi <hassanrandomz@gmail.com>
2023-04-20 14:50:24 +0500

    Add failing test for fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,4 +2,10 @@ require 'rspec'
 require_relative '../solver'
 
 describe Solver do
+  let(:solver) { Solver.new }
+  describe '#fizzbuzz' do
+    it 'returns fizz on 3' do
+      expect(solver.fizzbuzz(3)).to eq('fizz')
+    end
+  end
 end
